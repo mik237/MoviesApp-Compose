20f7da232999281c963726c07dc001963bf24bf8
muhammadibrahim <mik237@gmail.com>
2025-02-01 08:21:06 +0500

    MoviesListScreen fix UI

--- app/src/main/java/me/ibrahim/moviesapp/compose/presentation/movies_list/MoviesListScreen.kt
+++ app/src/main/java/me/ibrahim/moviesapp/compose/presentation/movies_list/MoviesListScreen.kt
@@ -8,17 +8,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import me.ibrahim.moviesapp.compose.R
 import me.ibrahim.moviesapp.compose.presentation.movies_list.components.MoviesList
@@ -43,9 +51,22 @@ fun MoviesListScreen(
                 .fillMaxSize()
                 .statusBarsPadding()
                 .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(25.dp)
+            verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
             Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = stringResource(id = R.string.title_movies_list_screen),
+                overflow = TextOverflow.Visible,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                maxLines = 2,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Start
+                )
+            )
 
             TitledMoviesList(title = stringResource(id = R.string.now_playing)) {
                 if (state.isLoading) {
@@ -55,7 +76,7 @@ fun MoviesListScreen(
                 }
             }
 
-            TitledMoviesList(title = stringResource(id = R.string.upcoming)) {
+            TitledMoviesList(title = stringResource(id = R.string.upcoming_movies)) {
                 if (state.isLoading) {
                     CircularProgressIndicator()
                 } else {
